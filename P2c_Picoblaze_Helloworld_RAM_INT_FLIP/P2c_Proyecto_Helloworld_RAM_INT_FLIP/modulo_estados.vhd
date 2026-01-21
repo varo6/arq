4ec10eb3899a7bc35024a2823efc05469c166df4
Andre Yermak Naumenko <asyermak@gmail.com>
2026-01-21 18:46:34 +0100

    feat/estados: modulo_estados list

--- P2c_Picoblaze_Helloworld_RAM_INT_FLIP/P2c_Proyecto_Helloworld_RAM_INT_FLIP/modulo_estados.vhd
+++ P2c_Picoblaze_Helloworld_RAM_INT_FLIP/P2c_Proyecto_Helloworld_RAM_INT_FLIP/modulo_estados.vhd
@@ -24,18 +24,17 @@ architecture Behavioral of estados is
     signal next_state    : state_type;
     
     -- 5 segundos a 50000000 MHz = 250000000 ciclos
-    signal timer_1s : std_logic_vector(25 downto 0); -- "10111110101111000010000000" = 50.000.000 = 1 segundo
-    signal count_5 : std_logic_vector(2 downto 0); -- "101" = 5
+    signal timer_1s : unsigned(25 downto 0); -- "10111110101111000010000000" = 50.000.000 = 1 segundo
+    signal count_5  : unsigned(2 downto 0); -- "101" = 5
     
-    signal flag : std_logic_vector(1 downto 0); -- "01" si viene de error, "10" si viene de acierto
+    signal flag : std_logic_vector(1 downto 0); -- "01" si viene de error, "10" si viene de acierto, "00" si acaba de arrancar
     signal timer_done : std_logic;
 begin
     process(clk, reset)
     begin
         if (reset = '1') then
             estado_actual <= estado_bloqueo;
-
-            
+            flag <= "00";
         elsif rising_edge(clk) then
             estado_actual <= next_state;
         end if;
@@ -73,28 +72,26 @@ begin
 
     process(estado_actual, write_strobe, port_id, out_port, timer_counter)
     begin
-        -- Valores por defecto (para evitar latches)
         next_state    <= estado_actual;
-        system_locked <= '0';
+        out_result <= "11111111";
 
         case estado_actual is
             
-            -- 1. ESTADO DE BLOQUEO (Esperando al Timer)
+            -- ESTADO DE BLOQUEO (Esperando al Timer)
             when estado_bloqueo =>
-                system_locked <= '1'; -- Salida a 1
+                out_result <= "100000" AND flag;
                 
-                -- Si el contador del otro proceso ha terminado, cambiamos
-                if timer_counter >= MAX_COUNT then
+                if rising_edge(timer_done) then
                     next_state <= estado_escucha;
                 else
                     next_state <= estado_bloqueo;
                 end if;
 
-            -- 2. ESTADO DE ESCUCHA
+            -- ESTADO DE ESCUCHA
             when estado_escucha =>
-                system_locked <= '0';
+                out_result <= "00000000";
                 
-                if (write_strobe = '1' and port_id = PORT_CMD) then
+                if (write_strobe = '1' and port_id = x"DD") then
                     if out_port = x"00" then      -- Acierto
                         next_state <= estado_bien;
                     elsif out_port = x"FF" then   -- Fallo
@@ -102,11 +99,12 @@ begin
                     end if;
                 end if;
 
-            -- 3. ERROR 1 (Quedan 2 intentos)
+            -- ERROR 1 (Quedan 2 intentos)
             when estado_error1 =>
-                system_locked <= '0';
+                flag <= "01";
+                out_result <= "00000001";
                 
-                if (write_strobe = '1' and port_id = PORT_CMD) then
+                if (write_strobe = '1' and port_id = x"DD") then
                     if out_port = x"00" then      -- Acierto
                         next_state <= estado_bien;
                     elsif out_port = x"FF" then   -- Fallo
@@ -114,11 +112,12 @@ begin
                     end if;
                 end if;
 
-            -- 4. ERROR 2 (Queda 1 intento)
+            -- ERROR 2 (Queda 1 intento)
             when estado_error2 =>
-                system_locked <= '0';
+                flag <= "01";
+                out_result <= "00000010";
                 
-                if (write_strobe = '1' and port_id = PORT_CMD) then
+                if (write_strobe = '1' and port_id = x"DD") then
                     if out_port = x"00" then      -- Acierto
                         next_state <= estado_bien;
                     elsif out_port = x"FF" then   -- Fallo
@@ -126,14 +125,16 @@ begin
                     end if;
                 end if;
 
-            -- 5. ERROR 3 (Fallo final -> Ir a bloqueo)
+            -- ERROR 3
             when estado_error3 =>
-                -- Transici�n incondicional al bloqueo
+                flag <= "01";
+                out_result <= "00000100";
                 next_state <= estado_bloqueo;
 
             -- 6. BIEN (Acierto -> Ir a bloqueo)
             when estado_bien =>
-                -- Transici�n incondicional al bloqueo
+                flag <= "10";
+                out_result <= "00001000";
                 next_state <= estado_bloqueo;
                 
         end case;
